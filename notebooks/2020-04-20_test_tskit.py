56d4a7a61bc2791591b80e582905119905f861ea
Daniel P. Rice <daniel.paul.rice@gmail.com>
2020-04-20 14:36:38 -0500

    Add notebook to test tskit.

---
+++ notebooks/2020-04-20_test_tskit.py
@@ -0,0 +1,112 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import msprime
+
+# ## Testing the tskit `allele_frequency_spectrum` method.
+
+random_seed = 100
+sample_size = 100
+num_replicates = 1000
+sims = msprime.simulate(sample_size=sample_size,
+                        num_replicates=num_replicates,
+                        random_seed=random_seed)
+afs = np.zeros(sample_size + 1)
+for tseq in sims:
+    afs += tseq.allele_frequency_spectrum(mode='branch')
+afs /= num_replicates
+plt.loglog(afs)
+
+# Find T2. It should be half the branch length of singletons when n=2
+
+sample_size = 2
+sims = msprime.simulate(sample_size=sample_size,
+                        num_replicates=num_replicates,
+                        random_seed=random_seed)
+afs = np.zeros(sample_size + 1)
+for tseq in sims:
+    afs += tseq.allele_frequency_spectrum(mode='branch')
+afs /= num_replicates
+pi = afs[1]
+T2 = pi / 2
+print(T2)
+
+# Simulate 100bp. Choose $r=0.1$ so that $1 / rT_2 = 10$:
+
+params = {
+    'sample_size': 2,
+    'length': 100,
+    'recombination_rate': 0.1,
+    'random_seed': 100,
+    'num_replicates': 10000,
+}
+d_c = 1.0 / params['recombination_rate']
+sims = msprime.simulate(**params)
+
+# Compute the branch length correlation as a function of distance.
+
+windows = np.arange(params['length'] + 1)
+corr = np.zeros(params['length'])
+for tseq in sims:
+    afs = tseq.allele_frequency_spectrum(mode='branch', windows=windows)
+    corr += afs[0, 1] * afs[:, 1]
+corr /= params['num_replicates']
+
+plt.plot(corr)
+plt.xlabel("Distance between sites")
+plt.ylabel(r"$\left< T_2^j T_2^k \right>$")
+plt.vlines([d_c], 4.0, 7.5, linestyle='dashed', label='$d_c$')
+plt.legend()
+plt.title("Decay in $T_2$ correlation")
+
+# Expand to larger samples
+
+
+def get_sfs(sims, sample_size, length):
+    """Compute the twosfs compared to left end of a sequence."""
+    windows = np.arange(length + 1)
+    onesfs = np.zeros((sample_size + 1))
+    twosfs = np.zeros((length, sample_size + 1, sample_size + 1))
+    n_sims = 0
+    for tseq in sims:
+        afs = tseq.allele_frequency_spectrum(mode='branch', windows=windows)
+        onesfs += np.mean(afs, axis=0)
+        twosfs += afs[0, :, None] * afs[:, None, :]
+        n_sims += 1
+    return onesfs / n_sims, twosfs / n_sims
+
+
+params = {
+    'sample_size': 4,
+    'length': 100,
+    'recombination_rate': 0.1,
+    'random_seed': 100,
+    'num_replicates': 10000,
+}
+d_c = 1.0 / params['recombination_rate']
+sims = msprime.simulate(**params)
+onesfs, twosfs = get_sfs(sims, params['sample_size'], params['length'])
+
+plt.plot(twosfs[:, 1, 1] - onesfs[1]**2, label=r'$a=1, b=1$')
+plt.plot(twosfs[:, 1, 2] - onesfs[1] * onesfs[2], label=r'$a=1, b=2$')
+plt.plot(twosfs[:, 2, 1] - onesfs[2] * onesfs[1], label=r'$a=2, b=1$')
+plt.plot(twosfs[:, 2, 2] - onesfs[2]**2, label=r'$a=2, b=2$')
+plt.xlabel("Distance between sites, $d$")
+plt.ylabel(
+    r"$\left< T_a^0 T_b^d \right> - \left< T_a \right> \left< T_b \right>$")
+plt.legend()
