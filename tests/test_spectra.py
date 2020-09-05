9464be3fb4adb55d51eea90454579ab0fc9fde7e
Daniel P. Rice <daniel.paul.rice@gmail.com>
2020-09-05 07:01:10 -0500

    Document tests.

--- tests/test_spectra.py
+++ tests/test_spectra.py
@@ -10,7 +10,10 @@ from twosfs import Spectra, avg_spectra, load_spectra, sfs2pi
 
 
 class TestSpectra(TestCase):
+    """Test the Spectra class."""
+
     def setUp(self):
+        """Set up a sample spectrum and some variables for comparison."""
         self.sample_size = 10
         self.length = 5
         self.recombination_rate = 1.0
@@ -20,10 +23,10 @@ class TestSpectra(TestCase):
             self.sfs, self.twosfs, recombination_rate=self.recombination_rate
         )
 
-    def test_dimesions(self):
+    def test_dimensions(self):
+        """Test that the provided spectra have the correct shape."""
         self.assertEqual(self.spectra.sample_size, self.sample_size)
         self.assertEqual(self.spectra.length, self.length)
-        # Test that input arrays must have the right shape
         with self.assertRaises(ValueError):
             Spectra(
                 np.empty((1, self.sample_size + 1)),
@@ -51,10 +54,18 @@ class TestSpectra(TestCase):
             )
 
     def test__init__recombination(self):
+        """Check that the recombination rate defaults to zero and takes value."""
         self.assertEqual(Spectra(self.sfs, self.twosfs).recombination_rate, 0.0)
         self.assertEqual(self.spectra.recombination_rate, self.recombination_rate)
 
     def test___init__normalization(self):
+        """
+        Test the normalization flag of the initializer.
+
+        - Sets t2 to provided value if normalized and t2 provided.
+        - Computes t2 from sfs if not normalized and t2 not provided.
+        - Raises exceptions in other cases.
+        """
         sfs_normed = self.sfs / np.sum(self.sfs)
         twosfs_normed = self.twosfs / np.sum(self.twosfs, axis=(1, 2))[:, None, None]
         # If normalized, set t2
@@ -77,6 +88,12 @@ class TestSpectra(TestCase):
             spectra = Spectra(sfs_normed, 2 * twosfs_normed, t2=1.0, normalized=True)
 
     def test___init__folded(self):
+        """
+        Test that the folded kwarg to init is working properly.
+
+        - Should set the folded attribute
+        - Should raise an exception when given unfolded spectra
+        """
         n = self.sample_size
         sfs_folded = np.ones(n + 1)
         sfs_folded[-(n + 1) // 2 :] = 0.0
@@ -90,6 +107,7 @@ class TestSpectra(TestCase):
             Spectra(self.sfs, self.twosfs, folded=True)
 
     def test___eq__(self):
+        """Test the equality relation."""
         a = Spectra(self.sfs, self.twosfs)
         b = Spectra(self.sfs.copy(), self.twosfs.copy())
         self.assertTrue(a == b)
@@ -97,6 +115,7 @@ class TestSpectra(TestCase):
         self.assertFalse(a == b)
 
     def test_save_load(self):
+        """Test that saving and loading returns equivalent spectra."""
         with TemporaryFile() as tf:
             self.spectra.save(tf)
             tf.seek(0)
@@ -104,6 +123,7 @@ class TestSpectra(TestCase):
         self.assertTrue(self.spectra == loaded_spectra)
 
     def test_export_to_fastNeutrino(self):
+        """Test that exporting fastNeutrino output matches expectation."""
         with NamedTemporaryFile() as tf:
             self.spectra.export_to_fastNeutrino(tf.name)
             tf.seek(0)
@@ -117,6 +137,7 @@ class TestSpectra(TestCase):
         )
 
     def test_normalize(self):
+        """Test that normalizing spectra makes them sum to one."""
         spectra = Spectra(self.sfs, self.twosfs)
         self.assertFalse(spectra.normalized)
         spectra.normalize()
@@ -125,6 +146,13 @@ class TestSpectra(TestCase):
         self.assertTrue(np.allclose(np.sum(spectra.twosfs, axis=(1, 2)), 1))
 
     def test_fold(self):
+        """
+        Testing that folding works properly.
+
+        - Preserves sums
+        - Zeros out high frequencies
+        - Gives correctly folded spectra
+        """
         self.spectra.fold()
         self.assertTrue(self.spectra.folded)
         # Folding preserves sums
@@ -157,6 +185,7 @@ class TestSpectra(TestCase):
         )
 
     def test_lumped_sfs(self):
+        """Test that lumped sfs preserve values and give the right shape."""
         kmax = 5
         lumped_sfs = self.spectra.lumped_sfs(kmax)
         self.assertEqual(lumped_sfs.shape, (kmax + 1,))
@@ -164,6 +193,7 @@ class TestSpectra(TestCase):
         self.assertEqual(np.sum(self.sfs), np.sum(lumped_sfs))
 
     def test_lumped_twosfs(self):
+        """Test that lumped twosfs preserve values and give the right shape."""
         kmax = 5
         lumped_twosfs = self.spectra.lumped_twosfs(kmax)
         self.assertEqual(lumped_twosfs.shape, (self.length, kmax + 1, kmax + 1))
@@ -177,7 +207,16 @@ class TestSpectra(TestCase):
 
 
 class TestAvgSpectra(TestCase):
+    """Test the avg_spectra function."""
+
     def test_avg_spectra(self):
+        """
+        Test that avg_spectra is working properly.
+
+        - Averages the branch length properties of two spectra.
+        - Rejects mismatches in recombination rate.
+        - Rejects normalized spectra.
+        """
         n = 4
         length = 2
         sfs1 = np.ones(n)
