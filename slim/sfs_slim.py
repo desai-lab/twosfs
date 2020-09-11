e8e6fdbaebd0c1450c2d5603f3015f82deb04c21
Daniel P. Rice <daniel.paul.rice@gmail.com>
2020-09-11 13:41:22 -0500

    Lint slim files.

--- slim/sfs_slim.py
+++ slim/sfs_slim.py
@@ -1,23 +1,25 @@
-import numpy as np
-import pyslim
-import sys
 import random
 
-def get_sfs(f_name, sample_size,length):
+import numpy as np
+import pyslim
+
+
+def get_sfs(f_name, sample_size, length):
 
     tseq = pyslim.load(f_name).simplify()
 
     windows = np.zeros(length + 3)
-    windows[1:-1] = 5e7 - np.floor(length/2) + np.arange(101)
+    windows[1:-1] = 5e7 - np.floor(length / 2) + np.arange(101)
     windows[-1] = 1e8
     samples = [random.sample(range(2000), k=sample_size)]
 
     onesfs = np.zeros([sample_size + 1])
     twosfs = np.zeros([length, sample_size + 1, sample_size + 1])
 
-    afs = tseq.allele_frequency_spectrum(sample_sets=samples, mode='branch', polarised=True, windows=windows)[1:-1,:]
+    afs = tseq.allele_frequency_spectrum(
+        sample_sets=samples, mode="branch", polarised=True, windows=windows
+    )[1:-1, :]
     onesfs += np.mean(afs, axis=0)
-    twosfs += afs[:,None,:]*afs[:,:,None]
+    twosfs += afs[:, None, :] * afs[:, :, None]
 
     return onesfs, twosfs
-
