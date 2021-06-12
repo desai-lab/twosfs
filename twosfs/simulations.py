9b8dcda398bcfe27f5701760666120f7303e9aff
Daniel P. Rice <daniel.paul.rice@gmail.com>
2021-06-12 16:04:11 -0400

    Improve simulation parameter passing.

--- twosfs/simulations.py
+++ twosfs/simulations.py
@@ -1,27 +1,70 @@
 """Helper functions for running msprime simulations."""
+import json
 from hashlib import blake2b
+from typing import Iterable
 
 import msprime
 import numpy as np
 from scipy.special import betaln
 
-from twosfs.spectra import add_spectra, spectra_from_TreeSequence
+from twosfs.demography import (
+    expected_t2_demography,
+    make_exp_demography,
+    make_pwc_demography,
+)
+from twosfs.spectra import Spectra, add_spectra, spectra_from_TreeSequence
 
 
-def simulate_spectra(parameters: dict):
-    """Run msprime simulations and return a Spectra object.
+def list_rounded_parameters(params: Iterable[float], ndigits: int = 2) -> list[float]:
+    """Round each parameter in params to ndigits places and return a list."""
+    return list(map(lambda x: round(x, ndigits), params))
 
-    Parameters
-    ----------
-    parameters : Dict
-        Parameters to pass to msprime
-    """
-    sims = msprime.sim_ancestry(**parameters)
-    windows = np.arange(parameters["length"] + 1)
-    return add_spectra(
-        spectra_from_TreeSequence(windows, parameters["recombination_rate"], tseq)
-        for tseq in sims
+
+def make_parameter_string(**params) -> str:
+    """Convert parameter dictionary to json string without whitespace."""
+    return json.dumps(params, separators=(",", ":"))
+
+
+def _dispatch_model(
+    model: str, model_parameters: dict
+) -> tuple[msprime.AncestryModel, msprime.Demography, float]:
+    if model == "const":
+        coal_model = msprime.StandardCoalescent()
+        demography = make_pwc_demography([], [])
+        t2 = expected_t2_demography(demography)
+    elif model == "exp":
+        coal_model = msprime.StandardCoalescent()
+        demography = make_exp_demography(
+            end_time=model_parameters["end_time"],
+            growth_rate=model_parameters["growth_rate"],
+        )
+        t2 = expected_t2_demography(demography)
+    elif model == "beta":
+        coal_model = msprime.BetaCoalescent(alpha=model_parameters["alpha"])
+        demography = None
+        t2 = expected_t2_beta(alpha=model_parameters["alpha"])
+    else:
+        raise ValueError("Invalid model {model}. Must be const, exp, pwc, or beta.")
+    return coal_model, demography, t2
+
+
+def simulate_spectra(
+    model: str,
+    model_parameters: dict,
+    msprime_parameters: dict,
+    scaled_recombination_rate: float,
+) -> Spectra:
+    """Simulate spectra using msprime coalescent simulations."""
+    coal_model, demography, t2 = _dispatch_model(model, model_parameters)
+    r = scaled_recombination_rate / (2 * t2)
+    sims = msprime.sim_ancestry(
+        model=coal_model,
+        demography=demography,
+        recombination_rate=r,
+        **msprime_parameters
     )
+    windows = np.arange(msprime_parameters["sequence_length"] + 1)
+    return add_spectra(spectra_from_TreeSequence(windows, r, tseq) for tseq in sims)
 
 
 def expected_t2_beta(alpha, pop_size=1.0):
