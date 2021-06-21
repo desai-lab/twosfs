f12535f9d1fbeeba65f94b658bc7edb2bf231585
Eliot Fenton <efenton@boslogin03.rc.fas.harvard.edu>
2021-06-21 17:58:37 -0400

    Updated SLiM code to remove magic numbers

--- slim/sfs_slim.py
+++ slim/sfs_slim.py
@@ -1,16 +1,13 @@
-import random
 import numpy as np
 import pyslim
 
-def get_tseq(f_name, sample_size, length=1e8, num_bp=100, num_trees=6, cut=.2):
+def get_tseq(f_name, samples, length=1e8, num_bp=100, num_trees=6, cut=.2):
 
     tseq = pyslim.load(f_name).simplify()
-
-    keep_nodes = random.sample(range(2000), sample_size)
-    tseq = tseq.simplify(keep_nodes)
+    tseq = tseq.simplify(samples)
 
     spacing = (1-2*cut)/num_trees
-    intervals = [[length*(cut+spacing*i), length*(cut+spacing*i)+num_bp] for i in range(num_trees)]
+    intervals = [[round(length*(cut+spacing*i)), round(length*(cut+spacing*i) + num_bp)] for i in range(num_trees)]
 
     tseq_array = []
     for i in range(num_trees):
